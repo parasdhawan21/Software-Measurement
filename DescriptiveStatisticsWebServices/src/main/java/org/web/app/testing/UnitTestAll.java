308964861671aecc4214b32d2618d1f4dce93fe4
PARAS DHAWAN <parasdhawan21@gmail.com>
2017-05-31 19:00:21 -0400

    Test case for checking range of random numbers

--- DescriptiveStatisticsWebServices/src/main/java/org/web/app/testing/UnitTestAll.java
+++ DescriptiveStatisticsWebServices/src/main/java/org/web/app/testing/UnitTestAll.java
@@ -12,67 +12,85 @@ import org.web.app.models.MeanCalculator;
 import org.web.app.models.MedianCalculator;
 import org.web.app.models.MinValueCalculator;
 import org.web.app.models.ModeCalculator;
+import org.web.app.models.RandomGenerator;
 import org.web.app.models.StandardDeviationCalulator;
 
 
+/**
+ * 
+ * Class to test the system
+ *
+ */
 public class UnitTestAll {
-	
-	private ArrayList<Integer>sampleArrayList;
-	private MeanCalculator  meanCalc;
+
+	private ArrayList<Integer> sampleArrayList;
+	private MeanCalculator meanCalc;
 	private MedianCalculator medianCalc;
 	private ModeCalculator modeCalc;
 	private MaxValueCalculator maxCalc;
 	private MinValueCalculator minCalc;
 	private StandardDeviationCalulator stdCalc;
-	
-	
+	private RandomGenerator randomGenerator;
+
 	@Before
 	public void initializeTests() {
-		sampleArrayList=new ArrayList<Integer>(Arrays.asList(67,74,58,24,8,99,15,51,34,63,98,59,95,26,91,21,
-											  4,88,18,25,56,87,65,48,84,43,74,57,88,52,35,95,25,8,6,90,20,51,
-											  82,99,29,89,72,55,86,21,28,64,28,11,28,46,82,41,10,13,97,8,44,
-											  57,51,34,46,67,84,53,89,48,31,62,45,4,93,52,72,89,72,31,40,23,6,
-											  42,49,32,15,75,5,41,0,31,50,51,1,6,71,30,66,25,8,92)) ;
-	
-		meanCalc=new MeanCalculator(sampleArrayList);
-		medianCalc=new MedianCalculator(sampleArrayList);
-		modeCalc=new ModeCalculator(sampleArrayList); 
-		maxCalc=new MaxValueCalculator(sampleArrayList);
-		minCalc=new MinValueCalculator(sampleArrayList);
-		stdCalc=new StandardDeviationCalulator(sampleArrayList,meanCalc.getMeanValue());
-		
+		sampleArrayList = new ArrayList<Integer>(Arrays.asList(67, 74, 58, 24, 8, 99, 15, 51, 34, 63, 98, 59, 95, 26,
+				91, 21, 4, 88, 18, 25, 56, 87, 65, 48, 84, 43, 74, 57, 88, 52, 35, 95, 25, 8, 6, 90, 20, 51, 82, 99, 29,
+				89, 72, 55, 86, 21, 28, 64, 28, 11, 28, 46, 82, 41, 10, 13, 97, 8, 44, 57, 51, 34, 46, 67, 84, 53, 89,
+				48, 31, 62, 45, 4, 93, 52, 72, 89, 72, 31, 40, 23, 6, 42, 49, 32, 15, 75, 5, 41, 0, 31, 50, 51, 1, 6,
+				71, 30, 66, 25, 8, 92));
+
+		meanCalc = new MeanCalculator(sampleArrayList);
+		medianCalc = new MedianCalculator(sampleArrayList);
+		modeCalc = new ModeCalculator(sampleArrayList);
+		maxCalc = new MaxValueCalculator(sampleArrayList);
+		minCalc = new MinValueCalculator(sampleArrayList);
+		stdCalc = new StandardDeviationCalulator(sampleArrayList, meanCalc.getMeanValue());
+
 	}
-	
-	
+
 	@Test
 	public void test100Values() {
-				
-		assertEquals(48.74, meanCalc.getMeanValue(),0);
-				
-		assertEquals(48.5, medianCalc.getMedianValue(),0);
-			
+
+		assertEquals(48.74, meanCalc.getMeanValue(), 0);
+
+		assertEquals(48.5, medianCalc.getMedianValue(), 0);
+
 		assertEquals(8, modeCalc.getModeValue());
-			
+
 		assertEquals(99, maxCalc.getMaxValue());
-			
+
 		assertEquals(0, minCalc.getMinValue());
-			
-		assertEquals(28.89, stdCalc.getStdValue(),0);
-		
+
+		assertEquals(28.89, stdCalc.getStdValue(), 0);
+
 	}
-	
-	
-	
+
+	/**
+	 * Test for range of Random Numbers
+	 */
+	@Test
+	public void testRandomNumbersRange() {
+		for (int i = 0; i < 1000; i++) {
+			randomGenerator = new RandomGenerator(100);
+			sampleArrayList = randomGenerator.getRandomArray();
+			minCalc = new MinValueCalculator(sampleArrayList);
+			maxCalc = new MaxValueCalculator(sampleArrayList);
+			assertEquals(0, minCalc.getMinValue());
+			assertEquals(99, maxCalc.getMaxValue());
+		}
+	}
+
 	@After
-	public void tearDown() {	
-		
-		sampleArrayList=null;
-		meanCalc=null;
-		medianCalc=null;
-		modeCalc=null;
-		maxCalc=null;
-		minCalc=null;
-		stdCalc=null;		
+	public void tearDown() {
+
+		sampleArrayList = null;
+		meanCalc = null;
+		medianCalc = null;
+		modeCalc = null;
+		maxCalc = null;
+		minCalc = null;
+		stdCalc = null;
 	}
 
 }
